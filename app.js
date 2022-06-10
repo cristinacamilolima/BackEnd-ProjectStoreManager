49443ff01268fb722a70bd9f1a463674325de89d
Cristina Camilo <cristinacamilo.lima@gmail.com>
2022-06-10 03:25:28 -0300

    requisito 3

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productController = require('./controllers/productController');
 const saleController = require('./controllers/saleController');
+const validator = require('./middlewares/validator');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,9 +17,13 @@ app.get('/products', productController.getAllProducts);
 
 app.get('/products/:id', productController.findProductById);
 
+app.post('/products', validator.isValidProduct, () => { console.log('teste'); });
+
 app.get('/sales', saleController.getAllSales);
 
 app.get('/sales/:id', saleController.findSaleById);
+
+app.post('/sales', validator.isValidSale, () => { console.log('testeSales'); });
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
---
+++ middlewares/validator.js
@@ -0,0 +1,41 @@
+const isValidProduct = (req, res, next) => {
+    const { name, quantity } = req.body;
+    
+    if (!name) {
+        return res.status(400).json({ message: '"name" is required' });
+    }
+    if (name.length < 5) {
+        return res.status(400).json({ 
+            message: '"name" length must be at least 5 characters long' });
+    }
+    if (quantity === undefined) {
+        return res.status(400).json({ message: '"quantity" is required' });
+    }
+    if (quantity <= 0) {
+        return res.status(400).json({ message: '"quantity" must be greater than or equal to 1' });
+    }
+    
+    next();
+};
+
+const isValidSale = (req, res, next) => {
+    const b = req.body;
+    
+    b.map((saleData) => {
+        const { productId, quantity } = saleData;
+        
+            if (!productId) {
+                return res.status(400).json({ message: '"productId" is required' });
+            }
+            if (quantity === undefined) {
+                return res.status(400).json({ message: '"quantity" is required' });
+            }
+            if (quantity <= 0) {
+                return res.status(400).json({ 
+                    message: '"quantity" must be greater than or equal to 1' });
+            }
+          return next();
+    });
+};
+
+module.exports = { isValidProduct, isValidSale };
